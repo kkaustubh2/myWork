1774ba487b65bde1c01ca3216bcb2dabec71479c
kaustubh <kaustubh@192.168.1.15>
2025-09-21 14:16:25 +0530

    lets go

--- js/numbers-maths/1_first.js
+++ js/numbers-maths/1_first.js
@@ -1,7 +1,6 @@
 let max = 57;
-let actual = max-13;
-let percentage = actual/max
-console.log(percentage)
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
 let x = +"";
 console.log(x);
-
--- js/strings/1_string.js
+++ js/strings/1_string.js
@@ -2,3 +2,7 @@ const one = "Hello,";
 const two = "How are you?";
 
 console.log(`${one} ${two}`);
+console.log(two.charAt(5));
+
+let x = one.at(3);
+console.log(x);
