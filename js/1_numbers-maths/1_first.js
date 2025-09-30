dc22d9529fa75243343779f2da81d7b2502fd426
kaustubh <kaustubh@192.168.1.15>
2025-09-30 22:55:52 +0530

    lol

--- js/1_numbers-maths/1_first.js
+++
@@ -1,6 +0,0 @@
-let max = 57;
-let actual = max - 13;
-let percentage = actual / max;
-console.log(percentage);
-let x = +"";
-console.log(x);
--- js/2_strings/1_string.js
+++
@@ -1,8 +0,0 @@
-const one = "Hello,";
-const two = "How are you?";
-
-console.log(`${one} ${two}`);
-console.log(two.charAt(5));
-
-let x = one.at(3);
-console.log(x);
